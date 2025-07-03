87252c9f64854b9cf6479cfe93f7e8262e1ae318
LucaSaiani <luca.saiani@gmail.com>
2025-07-03 17:34:38 +0100

    wip keymap

---
+++ icons/__init__.py
@@ -0,0 +1,24 @@
+import bpy
+import bpy.utils.previews
+from pathlib import Path
+
+pcoll = None
+
+
+def register():
+    global pcoll
+    pcoll = bpy.utils.previews.new()
+    dir = Path(__file__).parent
+    for img_file in dir.glob("*.png"):
+        pcoll.load(img_file.stem, str(img_file), 'IMAGE')
+
+
+def unregister():
+    global pcoll
+    bpy.utils.previews.remove(pcoll)
+
+
+def icon_id(name:str):
+    global pcoll
+    return pcoll[name].icon_id
+
