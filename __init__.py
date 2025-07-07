d37777426fe8afc4b80cc82f823697d639caf912
LucaSaiani <luca.saiani@gmail.com>
2025-07-07 09:15:23 +0100

    just a test

--- __init__.py
+++ __init__.py
@@ -18,10 +18,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-
-# import sys
-# import os
-
 if "bpy" in locals():
     import importlib
     importlib.reload(mastro_preferences),
