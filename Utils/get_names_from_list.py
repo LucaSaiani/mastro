ce4585a67e808d788702e4ad8e4d12617a6d83d8
Luca Saiani <luca.saiani@gmail.com>
2025-12-17 11:21:02 +0000

    updated how elements are listed in the drop down menu, to avoid bug with long names

--- Utils/get_names_from_list.py
+++ Utils/get_names_from_list.py
@@ -2,15 +2,17 @@ def get_names_from_list(scene, context, collection):
     """
     Generate a list of tuples (identifier, name, description) for EnumProperty
     from a Blender CollectionProperty.
-
-    :param scene: Blender scene
-    :param context: Blender context (required by EnumProperty)
-    :param collection: the name of the CollectionProperty as a string
-    :return: list of tuples suitable for EnumProperty items
     """
+    # items = []
+    # coll = getattr(scene, collection, [])
+    # for el in coll:  # prendi l’ordine originale
+    # # for el in getattr(scene, collection, []):
+    #     # items.append((el.name, el.name, "", el.id))
+    #     items.append((el.name, el.name, f"Id. {el.id} - {el.name}"))
+    # return items
+
     items = []
-    for el in getattr(scene, collection, []):
-        items.append((el.name, el.name, "", el.id))
-        
+    for el in sorted(getattr(scene, collection, []), key=lambda e: e.name):
+        items.append((el.name, el.name, f"Id. {el.id} - {el.name}"))
     return items
 
