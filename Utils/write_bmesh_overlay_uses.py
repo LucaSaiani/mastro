21cb29c2a72f88acac4a4dc9343626f8c7f8a679
Luca Saiani <luca.saiani@gmail.com>
2025-12-24 11:32:39 +0000

    Extended the top override function

--- Utils/write_bmesh_overlay_uses.py
+++ Utils/write_bmesh_overlay_uses.py
@@ -4,106 +4,108 @@ from .read_write_bmesh_use_attribute import write_bmesh_use_attribute
 
 def overlay_bmesh_uses(bm, selection, value, mode):
     if mode == "FACE":
-        bMesh_typology     = bm.faces.layers.int["mastro_typology_id"]
-        bmesh_overlay_top    = bm.faces.layers.int["mastro_overlay_top"]
+        bm.faces.ensure_lookup_table()
+        field = bm.faces
+        suffix = ""
     else:
-        bMesh_typology     = bm.faces.layers.int["mastro_typology_id_EDGE"]
-        bmesh_overlay_top   = bm.edges.layers.int["mastro_overlay_top_EDGE"]
+        bm.edges.ensure_lookup_table()
+        field = bm.edges
+        suffix = "_EDGE"
+
+    bMesh_typology = field.layers.int[f"mastro_typology_id{suffix}"]
+    # bmesh_overlay_top   = field.layers.int[f"mastro_overlay_top{suffix}"]
     
     typology_id = selection[bMesh_typology]
+
     list_of_layers = write_bmesh_use_attribute(bm, selection, typology_id, mode)
         
     storey_list_A = list_of_layers["storey_list_A"]
     storey_list_B = list_of_layers["storey_list_B"]
     use_id_list_A = list_of_layers["use_id_list_A"]
     use_id_list_B = list_of_layers["use_id_list_B"]
+   
+  #  selection[bmesh_overlay_top] = value
     
-    selection[bmesh_overlay_top] = value
+    # if value > 0:
+    # count how many liquid uses there are, if any
+    use_id_list_A = str(use_id_list_A)[1:]
+    use_id_list_B = str(use_id_list_B)[1:]
+    uses = [a + b for a, b in zip(use_id_list_A, use_id_list_B)]
     
-    if value > 0:
-        # count how many liquid uses there are, if any
-        use_id_list_A = str(use_id_list_A)[1:]
-        use_id_list_B = str(use_id_list_B)[1:]
-        uses = [a + b for a, b in zip(use_id_list_A, use_id_list_B)]
+    list_of_uses = bpy.context.scene.mastro_use_name_list
+    liquid_ids = [item.id for item in list_of_uses if getattr(item, "liquid", False)]
+    liquid_count = sum(1 for use in uses if int(use) in liquid_ids)
+    
+    if liquid_count > 0:
+        storeys_left = value
+        removed_storeys = 0
+        updated_storeys_per_use = 0
+        storey_list_A = str(storey_list_A)[1:]
+        storey_list_B = str(storey_list_B)[1:]
+        bMesh_storeys = [int(a + b) for a, b in zip(storey_list_A, storey_list_B)]
         
-        list_of_uses = bpy.context.scene.mastro_use_name_list
-        liquid_ids = [item.id for item in list_of_uses if getattr(item, "liquid", False)]
-        liquid_count = sum(1 for use in uses if int(use) in liquid_ids)
-        
-        if liquid_count > 0:
-            storeys_left = value
-            removed_storeys = 0
-            updated_storeys_per_use = 0
-            storey_list_A = str(storey_list_A)[1:]
-            storey_list_B = str(storey_list_B)[1:]
-            bMesh_storeys = [int(a + b) for a, b in zip(storey_list_A, storey_list_B)]
+        # reverse the lists since we need to start from the top
+        bMesh_storeys.reverse()
+        storey_list_A = storey_list_A[::-1]
+        storey_list_B = storey_list_B[::-1]
+        uses.reverse()
+        uses_to_remove = []
+        for index, use in enumerate(uses):
+            use_id = int(use)
+            storeys_per_use = bMesh_storeys[index]
             
-            # reverse the lists since we need to start from the top
-            bMesh_storeys.reverse()
-            storey_list_A = storey_list_A[::-1]
-            storey_list_B = storey_list_B[::-1]
-            uses.reverse()
-            uses_to_remove = []
-            for index, use in enumerate(uses):
-                use_id = int(use)
-                storeys_per_use = bMesh_storeys[index]
-                
-                if use_id in liquid_ids:
-                    is_liquid = True
-                else:
-                    is_liquid = False
-                
-                if not is_liquid:
-                    if storeys_per_use >= storeys_left:
-                        updated_storeys_per_use = storeys_per_use - storeys_left
-                        removed_storeys = removed_storeys + storeys_left
-                        if storeys_per_use == storeys_left:
-                            uses_to_remove.append(use)
-                        override_uses(bm, 
-                                      selection, 
-                                      mode,
-                                      uses_to_remove, 
-                                      updated_storeys_per_use, 
-                                      liquid_ids, 
-                                      removed_storeys)
-                        return
-                    else:
+            if use_id in liquid_ids:
+                is_liquid = True
+            else:
+                is_liquid = False
+            
+            if not is_liquid:
+                if storeys_per_use >= storeys_left:
+                    updated_storeys_per_use = storeys_per_use - storeys_left
+                    removed_storeys = removed_storeys + storeys_left
+                    if storeys_per_use == storeys_left:
                         uses_to_remove.append(use)
-                        storeys_left = storeys_left - storeys_per_use
-                        removed_storeys = removed_storeys + storeys_per_use
-                if is_liquid:
                     override_uses(bm, 
-                                  selection, 
-                                  mode, 
-                                  uses_to_remove, 
-                                  updated_storeys_per_use, 
-                                  liquid_ids, 
-                                  removed_storeys)
+                                    selection, 
+                                    mode,
+                                    uses_to_remove, 
+                                    updated_storeys_per_use, 
+                                    liquid_ids, 
+                                    removed_storeys)
                     return
+                else:
+                    uses_to_remove.append(use)
+                    storeys_left = storeys_left - storeys_per_use
+                    removed_storeys = removed_storeys + storeys_per_use
+            if is_liquid:
+                override_uses(bm, 
+                                selection, 
+                                mode, 
+                                uses_to_remove, 
+                                updated_storeys_per_use, 
+                                liquid_ids, 
+                                removed_storeys)
+                return
                     
 def override_uses(bm, selection, mode, uses_to_remove, updated_storeys_per_use, liquid_ids, removed_storeys):
     if mode == "FACE": # mastro mass
         bm.faces.ensure_lookup_table()
-        bMesh_storey_list_A = bm.faces.layers.int["mastro_list_storey_A"]
-        bMesh_storey_list_B = bm.faces.layers.int["mastro_list_storey_B"]
-        bMesh_use_list_A   = bm.faces.layers.int["mastro_list_use_id_A"]
-        bMesh_use_list_B   = bm.faces.layers.int["mastro_list_use_id_B"]
-        bMesh_height_A     = bm.faces.layers.int["mastro_list_height_A"]
-        bMesh_height_B     = bm.faces.layers.int["mastro_list_height_B"]
-        bMesh_height_C     = bm.faces.layers.int["mastro_list_height_C"]
-        bMesh_height_D     = bm.faces.layers.int["mastro_list_height_D"]
-        bMesh_height_E     = bm.faces.layers.int["mastro_list_height_E"]
+        field = bm.faces
+        suffix = ""
     else: # mastro block
         bm.edges.ensure_lookp_table()
-        bMesh_storey_list_A = bm.edges.layers.int["mastro_list_storey_A_EDGE"]
-        bMesh_storey_list_B = bm.edges.layers.int["mastro_list_storey_B_EDGE"]
-        bMesh_use_list_A   = bm.edges.layers.int["mastro_list_use_id_A_EDGE"]
-        bMesh_use_list_B   = bm.edges.layers.int["mastro_list_use_id_B_EDGE"]
-        bMesh_height_A     = bm.edges.layers.int["mastro_list_height_A_EDGE"]
-        bMesh_height_B     = bm.edges.layers.int["mastro_list_height_B_EDGE"]
-        bMesh_height_C     = bm.edges.layers.int["mastro_list_height_C_EDGE"]
-        bMesh_height_D     = bm.edges.layers.int["mastro_list_height_D_EDGE"]
-        bMesh_height_E     = bm.edges.layers.int["mastro_list_height_E_EDGE"]
+        field = bm.edges
+        suffix = "_EDGE"
+
+    bMesh_storey_list_A = field.layers.int[f"mastro_list_storey_A{suffix}"]
+    bMesh_storey_list_B = field.layers.int[f"mastro_list_storey_B{suffix}"]
+    bMesh_use_list_A   = field.layers.int[f"mastro_list_use_id_A{suffix}"]
+    bMesh_use_list_B   = field.layers.int[f"mastro_list_use_id_B{suffix}"]
+    bMesh_height_A     = field.layers.int[f"mastro_list_height_A{suffix}"]
+    bMesh_height_B     = field.layers.int[f"mastro_list_height_B{suffix}"]
+    bMesh_height_C     = field.layers.int[f"mastro_list_height_C{suffix}"]
+    bMesh_height_D     = field.layers.int[f"mastro_list_height_D{suffix}"]
+    bMesh_height_E     = field.layers.int[f"mastro_list_height_E{suffix}"]
 
     use_list_A = str(selection[bMesh_use_list_A])
     use_list_B = str(selection[bMesh_use_list_B])
